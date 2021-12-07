551665cbe96f63564ef59c7f0b8a3f657209d0bf
Tiago Esdras <tiagoesdrasvieira@gmail.com>
2021-12-07 19:32:02 -0300

    Colunas

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 void main(){
   runApp(MaterialApp(
     title: "Frases do dia",
-    home: Container(color: Colors.white,),
+    home: Column(children: <Widget>[
+      Text("Texto 1"),
+      Text("Texto 2"),
+      Text("Texto 3")
+    ],),
+    //home: Container(color: Colors.white,),
   ));
 }
